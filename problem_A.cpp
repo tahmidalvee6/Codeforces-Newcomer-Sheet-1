254213769f65b0690bf02998cdd68e997ef80f05
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2025-02-19 05:40:02 +0600

    Create problem_A.cpp

---
+++ problem_A.cpp
@@ -0,0 +1,26 @@
+/*
+Given a name S. Print "Hello, (name)" without parentheses.
+
+Input
+Only one line containing a string S.
+
+Output
+Print "Hello, " without quotes, then print name.
+
+Example
+InputCopy
+programmer
+OutputCopy
+Hello, programmer
+*/
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    string s;
+    cin>>s;
+ 
+    cout<<"Hello, "<<s<<endl;
+    
+}
