b57cbc8aea7016d28c6c080b69c02a35d2ceaed7
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2025-02-25 05:55:40 +0600

    Create problem_H.cpp

---
+++ problem_H.cpp
@@ -0,0 +1,64 @@
+/*
+Given 2 numbers A
+ and B
+. Print floor, ceil and round of A/B
+
+Note:
+
+Floor: Is a mathematical function that takes a real number X
+ and its output is the greatest integer less than or equal to X
+.
+Ceil: Is a mathematical function that takes a real number X
+ and its output is the smallest integer larger than or equal to X
+.
+Round: Is a mathematical function that takes a real number X
+ and its output is the closest integer to that number X
+.
+
+For more clarification visit the links in the notes below.
+
+Input
+Only one line containing two numbers A
+ and B
+ (1≤A,B≤103)
+
+Output
+Print 3 lines that contain the following in the same order:
+
+"floor A
+ / B
+ = Floor result" without quotes.
+"ceil A
+ / B
+ = Ceil result" without quotes.
+"round A
+ / B
+ = Round result" without quotes.
+Examples
+InputCopy
+10 3
+OutputCopy
+floor 10 / 3 = 3
+ceil 10 / 3 = 4
+round 10 / 3 = 3
+InputCopy
+10 4
+OutputCopy
+floor 10 / 4 = 2
+ceil 10 / 4 = 3
+round 10 / 4 = 3
+InputCopy
+10 6
+OutputCopy
+floor 10 / 6 = 1
+ceil 10 / 6 = 2
+round 10 / 6 = 2
+Note
+Links:
+
+For Rounding method visit: https://www.mathsisfun.com/numbers/rounding-methods.html.
+For Flooring and Ceiling method visit: https://www.mathsisfun.com/sets/function-floor-ceiling.html.
+*/
+
+
+
